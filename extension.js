20f804c72bc4deafed88d855d71e60b6e1e0167d
CastilloDel <delcastillodelarosadaniel@gmail.com>
2023-11-28 23:27:16 +0100

    Draw an icon that shows the remaining time

--- extension.js
+++ extension.js
@@ -7,16 +7,33 @@ import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js';
 
 
+const repaint = (area, percentageDone) => {
+    let context = area.get_context();
+    const [width, height] = area.get_surface_size();
+    const x =  width / 2;
+    const y = height / 2;
+    const r = width / 2.5;
+    context.arc(x, y, r, 0, 2 * Math.PI);
+    context.setSourceRGBA(148 / 255, 148 / 255, 148 / 255, 1);
+    context.stroke();
+    const angleDone = 2 * Math.PI * percentageDone;
+    const startPoint = 1.5 * Math.PI;
+    const endPoint = (1.5 * Math.PI + angleDone) % (2 * Math.PI);
+    context.arc(x, y, r, startPoint, endPoint);
+    context.setSourceRGBA(242 / 255, 242 / 255, 242 / 255, 1);
+    context.stroke();
+
+    context.$dispose();
+}
+
 export default class SimpleBreakReminder extends Extension {
     enable() {
         // Create a panel button
         this._indicator = new PanelMenu.Button(0.0, this.metadata.name, false);
 
         // Add an icon
-        const icon = new St.Icon({
-            icon_name: 'face-laugh-symbolic',
-            style_class: 'system-status-icon',
-        });
+        const icon = new St.DrawingArea({ width: 25, height: 25 });
+        icon.connect('repaint', (area) => repaint(area, this.calculatePercentageDone()));
         this._indicator.add_child(icon);
 
          // Add a menu item to open the preferences window
@@ -34,9 +51,9 @@ export default class SimpleBreakReminder extends Extension {
         Main.panel.addToStatusArea(this.uuid, this._indicator);
 
         this.resetTimer();
-        GLib.timeout_add_seconds(GLib.PRIORITY_DEFAULT, 1, () => {
-            console.log((this._timeOutTime - new Date()) /1000 / 60);
+        GLib.timeout_add_seconds(GLib.PRIORITY_DEFAULT, 5, () => {
             this.check();
+            icon.queue_repaint();
             return GLib.SOURCE_CONTINUE;
         });
     }
@@ -54,6 +71,13 @@ export default class SimpleBreakReminder extends Extension {
         this._timeOutTime = new Date();
         // SetMinutes manages the overflow 
         this._timeOutTime.setMinutes(this._timeOutTime.getMinutes() + time);
+    }
+
+    calculatePercentageDone() {
+        const remainingMinutes = (this._timeOutTime - new Date()) / 1000 / 60;
+        const time = this._settings.get_uint('time');
+        const remainingPercentage = remainingMinutes / time;
+        return 1 - remainingPercentage;
     }
 
     disable() {
